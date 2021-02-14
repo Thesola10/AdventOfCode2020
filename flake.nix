19d7945eef7c9209021eba6598296ac4cb707211
TheSola10 <thesola10@bobile.fr>
2021-02-14 22:27:55 +0100

    Made into Nix Flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Advent of Code 2020 solutions";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        { devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
